d0777d86e514550486cf56ba70f18ca3fe7f1d92
Carlos Jenkins <carlos@jenkins.co.cr>
2016-08-27 13:23:58 -0600

    Finished data processing.

--- api/README.rst
+++ api/README.rst
@@ -1,9 +1,9 @@
 Taximetro API
 =============
 
-1) Install Python 3 and Pip:
+1) Install MySQL, Python 3 and Pip:
 
-    sudo apt-get install python3
+    sudo apt-get install python3 mysql-server-5.6
     wget https://bootstrap.pypa.io/get-pip.py
     sudo python3 get-pip.py
 
@@ -11,15 +11,28 @@ Taximetro API
 
     sudo pip3 install -r requirements.txt
 
-3) Run API:
+3) Create data schema and data.
+
+    pushd data
+    python3 extractor.py
+    popd
+
+4) Create user and database:
+
+    $ mysql -u root -p
+    mysql> source schema_and_data.sql;
+    mysql> GRANT ALL PRIVILEGES ON taximetro.* TO 'taximetro'@'localhost' IDENTIFIED BY 'taximetro';
+    mysql> FLUSH PRIVILEGES;
+
+4) Run API:
 
     FLASK_APP=taximetro.py python3 -m flask run
 
-4) In case you require to inspect the database you can get it from:
+
+Note: In case you require to inspect the database you can get it from:
 
     http://datos.ctp.go.cr/dashboards/9261/taxis/
 
    The database can be downloaded as CSV using the following url:
 
     http://datos.ctp.go.cr/rest/datastreams/91182/data.csv
-
--- api/data/extractor.py
+++ api/data/extractor.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from os import getcwd
 from json import dumps
 from csv import DictReader
 from os.path import basename, join, isfile
-from pprint import pprint
 
 from requests import get
 
@@ -31,6 +32,26 @@ def download_file(url, outfile):
     return outfile
 
 
+def format_sql_insert(row):
+    """
+    Row is in the form:
+
+    {'base': '000000 - Am San José',
+     'idnum': '6-0244-0418',
+     'lastname1': 'Apuy',
+     'lastname2': 'Gonzalez',
+     'name': 'Gerardo',
+     'plate': 'TSJ-000138',
+     'service': 'Sedan'}
+    """
+    return (
+        'INSERT INTO `taxi` '
+        '(base, idnum, lastname1, lastname2, name, plate, service) VALUES '
+        '("{base}", "{idnum}", "{lastname1}", "{lastname2}", '
+        '"{name}", "{plate}", "{service}");'
+    ).format(**row)
+
+
 def main():
 
     # Download data file if required
@@ -42,6 +63,7 @@ def main():
 
     # Read CSV data
     bad_data = []
+    good_data = []
 
     with open(data_file, 'r') as infd:
         headers = infd.readline().strip().split(',')
@@ -52,18 +74,49 @@ def main():
         for data_row in reader:
 
             # Check for bad data in the set
-            if not data_row['plate']:
+            if not all(data_row[key] for key in ['plate', 'idnum', 'name']):
                 bad_data.append(data_row)
                 continue
 
             # Change data presentation
             for key in TITLE_KEYS:
                 data_row[key] = data_row[key].title()
-            pprint(data_row)
+            good_data.append(data_row)
 
-    bad_data_file = join(getcwd(), 'bad.json')
-    with open(bad_data_file, 'w') as badfd:
-        badfd.write(dumps(bad_data, indent=4))
+    # Write and report bad data
+    if bad_data:
+        print('WARNING: Found {} bad entries.'.format(len(bad_data)))
+
+        bad_data_file = join(getcwd(), 'bad.json')
+        with open(bad_data_file, 'w') as badfd:
+            badfd.write(dumps(bad_data, indent=4))
+
+        print('Bad data entries saved to {}'.format(bad_data_file))
+
+    # Grab database schema
+    with open('schema.sql', 'r') as fd:
+        schema = fd.read()
+
+    data_spot = schema.find('SET SQL_MODE=@OLD_SQL_MODE;')
+    schema_header = schema[:data_spot]
+    schema_footer = schema[data_spot:]
+
+    # Write final schema
+    schema_and_data = join(getcwd(), 'schema_and_data.sql')
+    print('Writing schema, please wait...')
+
+    with open(schema_and_data, 'w') as fd:
+        fd.write(schema_header)
+        fd.write('-- BEGIN INSERT DATA\n\n')
+        fd.write('SET AUTOCOMMIT=0;\n')
+        for row in good_data:
+            fd.write(format_sql_insert(row))
+            fd.write('\n')
+        fd.write('COMMIT;\n')
+        fd.write('\n\n-- END INSERT DATA\n\n')
+        fd.write(schema_footer)
+
+    print('Database schema and data written to {}'.format(schema_and_data))
 
 
 if __name__ == '__main__':
