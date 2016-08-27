7991bd62426a2ccd57f30120efbe790e47ef90db
Carolina Aguilar <caroagse@gmail.com>
2016-08-27 10:58:47 -0600

    Moved styles to a separate folder.

--- web/src/index.js
+++ web/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
-import './index.css';
+import './styles/index.css';
 
 ReactDOM.render(
   (<Router history={browserHistory}>
