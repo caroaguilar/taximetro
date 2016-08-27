8342409d059069f62c626818a346066bec31b87b
Carolina Aguilar <caroagse@gmail.com>
2016-08-27 15:08:27 -0600

    Render reviews and reviews card.

---
+++ web/src/Review.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import StarRating from 'react-star-rating-component';
+import _ from 'underscore';
+
+import api from './api/index';
+import './styles/reviews.css';
+
+import profileImage from './images/profile-image.svg'
+
+class ReviewsCard extends Component {
+    render() {
+        console.log(this.props.review)
+        return (
+            <div className="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--2dp">
+
+            </div>
+        );
+    }
+}
+
+export default ReviewsCard;
--- web/src/ReviewsCard.js
+++ web/src/ReviewsCard.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import StarRating from 'react-star-rating-component';
+import _ from 'underscore';
+import Review from './Review';
 
 import api from './api/index';
-import './styles/driver.css';
+import './styles/reviews.css';
 
 import profileImage from './images/profile-image.svg'
 
@@ -10,8 +12,21 @@ class ReviewsCard extends Component {
     render() {
         console.log(this.props.reviews)
         return (
-            <div className="mdl-cell mdl-cell--6-col mdl-card mdl-shadow--2dp">
-
+            <div className="review-card mdl-cell mdl-cell--6-col mdl-card mdl-shadow--2dp">
+                <div className="mdl-card__title">
+                    <h2 className="mdl-card__title-text">Reviews</h2>
+                </div>
+                <div className="mdl-card__supporting-text">
+                    {_.isEmpty(this.props.reviews) &&
+                        <h2 className="mdl-typography--headline">
+                            El taxista aún no posee reviews.
+                            Sé el primero en calificarlo!
+                        </h2>
+                    }
+                    {_.map(this.props.reviews, function(r) {
+                        return <Review review={r}/>;
+                    })}
+                </div>
             </div>
         );
     }
