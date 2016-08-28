453d0ca6ecfad8a1c504584053f6483c33577343
Carolina Aguilar <caroagse@gmail.com>
2016-08-27 18:28:43 -0600

    Added link to facebook profile in reviewer name.

--- web/src/Review.js
+++ web/src/Review.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import StarRating from 'react-star-rating-component';
-import _ from 'underscore';
 
 import './styles/reviews.css';
-
-import profileImage from './images/profile-image.svg'
 
 class ReviewsCard extends Component {
     render() {
         var review = this.props.review;
         return (
             <div className="review-row mdl-cell mdl-cell--12-col mdl-card mdl-shadow--2dp">
-                <h3 className="review-name mdl-typography--headline">{review.fbname}</h3>
+                <a className="review-name mdl-button mdl-js-button mdl-button--primary mdl-typography--headline "
+                    href={`https://www.facebook.com/profile.php?id=${review.fbid}`}>
+                    {review.fbname}
+                </a>
+                <StarRating name={"rating"} value={review.rating} editing={false}/>
                 <p>{review.content}</p>
                 <span>{`Likes: ${review.likes}`}</span>
             </div>
         );
     }
 }
+
 
 export default ReviewsCard;
