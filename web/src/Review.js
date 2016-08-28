6b3e0b5463f9e6b7d359da7484eca5e4ab6ee340
Carlos Jenkins <carlos@jenkins.co.cr>
2016-08-27 18:38:39 -0600

    Added Facebook picture to reviews.

--- web/src/Review.js
+++ web/src/Review.js
@@ -10,6 +10,7 @@ class ReviewsCard extends Component {
             <div className="review-row mdl-cell mdl-cell--12-col mdl-card mdl-shadow--2dp">
                 <a className="review-name mdl-typography--headline "
                     href={`https://www.facebook.com/profile.php?id=${review.fbid}`}>
+                    <img className="review-user-image" src={`http://graph.facebook.com/${review.fbid}/picture?type=square`}/>
                     {review.fbname}
                 </a>
                 <StarRating name={"rating"} value={review.rating} editing={false}/>
